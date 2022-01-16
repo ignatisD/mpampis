0b2527c1be3ba43e1f52084bb9560407cf76e31f
iggi <ignatisnb@gmail.com>
2022-01-17 00:14:56 +0200

    Add context menu

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -2,14 +2,12 @@ if(typeof module === 'object'){
     window.module = module;
     module = undefined;
 }
-// require('electron-connect').client.create();
 
 const { exec, spawn } = require('child_process');
 const { shell } = require('electron');
 const { dialog } = require('electron').remote;
 const fs = require('fs');
 const path = require('path');
-const { clipboard } = require('electron');
 
 var defaultEnv = {
     directory: ""
